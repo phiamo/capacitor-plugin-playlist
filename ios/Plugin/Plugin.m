aa4fc4c96a1143f2162ace31b2852641974a8821
Philipp Mohrenweiser <phiamo@googlemail.com>
2021-10-23 21:07:44 +0200

    v0.1.16

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -3,7 +3,7 @@
 
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(PlaylistPlugin, "PlaylistPlugin",
+CAP_PLUGIN(PlaylistPlugin, "Playlist",
    CAP_PLUGIN_METHOD(setOptions, CAPPluginReturnPromise);
    CAP_PLUGIN_METHOD(initialize, CAPPluginReturnPromise);
    CAP_PLUGIN_METHOD(release, CAPPluginReturnPromise);
