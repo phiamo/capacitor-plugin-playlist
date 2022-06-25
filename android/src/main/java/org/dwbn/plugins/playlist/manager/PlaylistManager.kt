9a3db8bd4b44094aa04e33fdeefe06880f88a9af
Emil Arakelian <emilarakelian@gmail.com>
2022-06-25 14:05:58 +0200

    Fix changing playback rate on Android

--- android/src/main/java/org/dwbn/plugins/playlist/manager/PlaylistManager.kt
+++ android/src/main/java/org/dwbn/plugins/playlist/manager/PlaylistManager.kt
@@ -254,9 +254,9 @@ class PlaylistManager(application: Application) :
 
     fun setPlaybackSpeed(@FloatRange(from = 0.0, to = 1.0) speed: Float) {
         playbackSpeed = speed
-        if (currentMediaPlayer != null && currentMediaPlayer!!.get() != null && currentMediaPlayer!!.get() is AudioApi) {
+        if (playlistHandler!!.currentMediaPlayer != null &&  playlistHandler!!.currentMediaPlayer!! is AudioApi) {
             Log.i(TAG, "setPlaybackSpeed completing with speed = $speed")
-            (currentMediaPlayer!!.get() as AudioApi?)!!.setPlaybackSpeed(playbackSpeed)
+            (playlistHandler!!.currentMediaPlayer as AudioApi?)!!.setPlaybackSpeed(playbackSpeed)
         }
     }
 
