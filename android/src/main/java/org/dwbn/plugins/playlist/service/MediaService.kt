4a46b2bf2a888d7cf6d6ff16027dc26bfef2b3e4
Philipp Mohrenweiser <phiamo@googlemail.com>
2025-12-28 19:55:02 +0100

    chore: bump version to 0.8.5 and enhance MediaService with foreground service handling

--- android/src/main/java/org/dwbn/plugins/playlist/service/MediaService.kt
+++ android/src/main/java/org/dwbn/plugins/playlist/service/MediaService.kt
@@ -1,8 +1,11 @@
 package org.dwbn.plugins.playlist.service
 
 import android.app.Notification
+import android.app.Service
+import android.content.Intent
 import android.content.pm.ServiceInfo
 import android.os.Build
+import android.util.Log
 import com.devbrackets.android.playlistcore.components.playlisthandler.PlaylistHandler
 import com.devbrackets.android.playlistcore.service.BasePlaylistService
 import org.dwbn.plugins.playlist.App
@@ -17,6 +20,10 @@ import org.dwbn.plugins.playlist.service.MediaImageProvider.OnImageUpdatedListen
  * the application specific information required.
  */
 class MediaService : BasePlaylistService<AudioTrack, PlaylistManager>() {
+    companion object {
+        private const val TAG = "MediaService"
+    }
+
     override fun onCreate() {
         super.onCreate()
         // Adds the audio player implementation, otherwise there's nothing to play media with
@@ -35,17 +42,38 @@ class MediaService : BasePlaylistService<AudioTrack, PlaylistManager>() {
         playlistManager.mediaPlayers.clear()
     }
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return try {
+            super.onStartCommand(intent, flags, startId)
+        } catch (e: android.app.ForegroundServiceStartNotAllowedException) {
+            // Android 12+ blocks foreground service start from background
+            // Log the error but don't crash - service can still function without foreground status
+            Log.w(TAG, "Cannot start foreground service: app is in background", e)
+            // Return START_NOT_STICKY so service won't be restarted if killed
+            // The service will continue as a regular service (may be killed by system)
+            Service.START_NOT_STICKY
+        }
+    }
+
     override fun runAsForeground(notificationId: Int, notification: Notification) {
         if (inForeground) {
             return
         }
 
         inForeground = true
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
-            startForeground(notificationId, notification)
-        } else {
-            startForeground(notificationId, notification,
-                ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK)
+        try {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+                startForeground(notificationId, notification)
+            } else {
+                startForeground(notificationId, notification,
+                    ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK)
+            }
+        } catch (e: android.app.ForegroundServiceStartNotAllowedException) {
+            // Android 12+ blocks foreground service start from background
+            // Log the error but don't crash - service can still function without foreground status
+            Log.w(TAG, "Cannot start foreground service: app is in background", e)
+            inForeground = false
+            // Service will continue as a regular service (may be killed by system)
         }
     }
 
